2b6f97fce3fa95ae1f2087c8ddde1cc80ccc0f75
Philipp Rapp <philipprapp@gmx.de>
2024-05-20 10:49:46 +0200

    Added capturing example

---
+++ camera/capture_10sec_video.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import time
+import sys
+
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+
+print(sys.executable)
+
+picam2 = Picamera2()
+video_config = picam2.create_video_configuration()
+picam2.configure(video_config)
+
+encoder = H264Encoder(10000000)
+
+picam2.start_recording(encoder, 'test.h264')
+time.sleep(10)
+picam2.stop_recording()
